133f8268de5a5f1a21e62f435ced532884e49deb
Edy Segura <edysegura@gmail.com>
2020-03-16 06:45:19 -0300

    Define precache resources location

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,23 @@
+const cacheName = 'static-assets-v1'
+const assetsLocation = [
+  '/',
+  '/index.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+]
+
+async function precache() {
+  const cache = await caches.open(cacheName)
+  return cache.addAll(assetsLocation)
+}
+
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] installing service worker...', event)
 })
